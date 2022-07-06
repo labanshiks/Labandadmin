ade4b4010b4820c4a0bd1715bbc5ed5f95896f00
labanshiks <labanshiks4@gmail.com>
2022-07-06 17:38:58 +0300

    finished the landing page

---
+++ src/dummyData.js
@@ -0,0 +1,42 @@
+export const userData = [
+        {
+            name: "Jan",
+            "Active User": 4000,
+        },
+        {
+            name: "Feb",
+            "Active User": 3500,
+        },
+        {
+            name: "Mar",
+            "Active User": 3000,
+        },
+        {
+            name: "Jun",
+            "Active User": 3700,
+        },
+        {
+            name: "Jul",
+            "Active User": 4200,
+        },
+        {
+            name: "Aug",
+            "Active User": 4100,
+        },
+        {
+            name: "Sept",
+            "Active User": 4400,
+        },
+        {
+            name: "Oct",
+            "Active User": 5000,
+        },
+        {
+            name: "Nov",
+            "Active User": 5100,
+        },
+        {
+            name: "Dec",
+            "Active User": 5500,
+        },
+    ]
